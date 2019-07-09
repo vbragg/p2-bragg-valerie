bd4b651192448733b5c93a4b106d14042cb26f35
Valerie Bragg <vbragg@ufledu>
2019-07-08 22:58:43 -0400

    Add basic map to thrift page using API

---
+++ js/map.js
@@ -0,0 +1,17 @@
+/*var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 27.510110, lng: -99.508581},
+          zoom: 15
+        });
+      } */
+
+function initMap() {
+  // The location of VSTN
+  var volunteers = {lat: 27.510110, lng: -99.508581};
+  // The map, centered at VSTN
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 15, center: volunteers});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: volunteers, map: map});
+}
